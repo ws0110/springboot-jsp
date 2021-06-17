8280060c98da37cb88cbc3c1bbce17ebb020fa34
wisu jang <>
2021-06-17 20:46:51 +0900

    sh update

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,10 +5,6 @@ BUILD_PATH=/home/ec2-user/build
 TOMCAT_PATH=/home/ec2-user/apache-tomcat-9.0.45
 DEPLOY_PATH=$TOMCAT_PATH/webapps
 
-
-echo "> Backup Old War..."
-cp $DEPLOY_PATH/ROOT.war $DEPLOY_PATH/ROOT.war.BACK
-
 echo "> Tomcat pid check..."
 CURRENT_PID=$(pgrep -f tomcat)
 echo "$CURRENT_PID"
